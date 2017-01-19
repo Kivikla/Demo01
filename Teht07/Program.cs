52952af3017d78a630ec335dabceec900d91e0c6
K2557 <lasse.kivikas@gmail.com>
2017-01-19 10:44:15 +0200

    Seiska

---
+++ Teht07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vuosi;
+            Console.WriteLine("Anna vuosi: ");
+
+        }
+    }
+}
